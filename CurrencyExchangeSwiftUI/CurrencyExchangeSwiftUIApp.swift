fb45be65483f8ba7ac58ad2fa0f9e2328ade9dc1
Sasha <szhukov.me@gmail.com>
2024-09-13 20:20:04 +0400

    Initial Commit

---
+++ CurrencyExchangeSwiftUI/CurrencyExchangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeSwiftUIApp.swift
+//  CurrencyExchangeSwiftUI
+//
+//  Created by Sasha on 13.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
